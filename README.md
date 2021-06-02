146da0d4b48d1c951c75fcbb30664cb2f7528466
Donovon <donovonjbacon@gmail.com>
2021-06-02 14:23:32 -0400

    Tweaked output to get all upcoming trains + return in BART format

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Python scripts related to transit (L trains) in Chicago
 ## Overview
 - Python implementation of the [CTA Train Tracker API](https://www.transitchicago.com/developers/ttdocs/)
 - Goal is to build out useful scripts + LED display setup to show upcoming trains relevant to me (Damen blue line stop)
+- Output is in the form of BART's station displays example:
+
+O'Hare 5,13 MIN  
+Forest Park 8,17 MIN
 
 ### Requirements
 - Get API key from [here](https://www.transitchicago.com/developers/traintrackerapply/) and add it as the 'key' in the payload. 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 # Upcoming arrival info for specific stop (Damen)
 arrival_url = 'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx'
 payload = {
-    'key':'insert_key_here',
+    'key':'',
     'mapid':'40590',
     'outputType':'JSON'
     }
@@ -12,14 +12,24 @@ r = requests.get(arrival_url, params=payload)
 full_response = r.json()
 
 parsed_data = full_response['ctatt'].get('eta')
+ohare_trains = []
+forest_park_trains = []
+now = datetime.datetime.now() - datetime.timedelta(hours=1)
 
-next_train = parsed_data[0]
-following_train = parsed_data[1]
-date_time_next = datetime.datetime.strptime(next_train['arrT'], '%Y-%m-%dT%H:%M:%S')
-date_time_following = datetime.datetime.strptime(following_train['arrT'], '%Y-%m-%dT%H:%M:%S')
+for item in parsed_data:
+    if 'O\'Hare' in item.get('stpDe'):
+        arrival_time = datetime.datetime.strptime(item.get('arrT'), '%Y-%m-%dT%H:%M:%S')
+        difference = arrival_time - now
+        ohare_trains.append(difference.seconds // 60)
+    if 'Forest' in item.get('stpDe'):
+        arrival_time = datetime.datetime.strptime(item.get('arrT'), '%Y-%m-%dT%H:%M:%S')
+        difference = arrival_time - now
+        forest_park_trains.append(difference.seconds // 60)
 
-print('Next train: ' + next_train['stpDe'])
-print('Arriving at: ', date_time_next.strftime("%I:%M %p"))
-print('\n')
-print('Following train: ' + following_train['stpDe'])
-print('Arriving at: ', date_time_following.strftime("%I:%M %p"))
+#print in BART format
+print('O\'Hare', end=' ')
+print(','.join(map(str, ohare_trains)), end = ' ')
+print('MIN')
+print('Forest Park', end=' ')
+print(','.join(map(str, forest_park_trains)), end = ' ')
+print('MIN')
